4d43e673c932911013aa10b7b0fa7345f55a2f72
Tomasz Dyngosz <tom.dyngosz@gmail.com>
2022-01-05 01:29:57 +0100

    server boilerplate

--- server/api.js
+++ server/api.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const app = require('../server');
 const apiRouter = express.Router();
 
+app.get('/api/minions', (req, res, next) => {
 
+})
 
 module.exports = apiRouter;
